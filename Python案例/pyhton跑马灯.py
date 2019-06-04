07562e902a152eaeffc208da740acbc60883cdc2
FioraLove <51014054+FioraLove@users.noreply.github.com>
2019-06-04 10:59:16 +0800

    跑马灯小案例

---
+++ Python案例/pyhton跑马灯.py
@@ -0,0 +1,10 @@
+import os ,time
+def main():
+    content='北京欢迎你为你开天辟地。。。。。'
+    while True:
+        os.system('cls')
+        print(content)
+        time.sleep(0.5)
+        content = content[1:]+content[0]
+if __name__ == '__main__':
+    main()
