62ca4e54aca6635fc2293d99c3bdef3b012364a7
FioraLove <51014054+FioraLove@users.noreply.github.com>
2019-06-08 14:09:20 +0800

    Update python扫码签到系统.py

--- python扫码签到系统/python扫码签到系统.py
+++ python扫码签到系统/python扫码签到系统.py
@@ -2,7 +2,13 @@ from flask import Flask
 # 导入这个库，新建一个名为“templates”的directory。只能命名为templates
 from flask import render_template
 from flask import request
+import csv
+
 app = Flask(__name__)
+
+with open('签到表单.csv', 'a', newline='') as f:
+    csvwriter = csv.writer(f, dialect='excel')
+    csvwriter.writerow(['姓名', '性别', '年龄', '电话'])
 
 
 # 路由
@@ -18,12 +24,17 @@ def load():
 
 @app.route('/m')
 def cg():
-    name=request.args.get("name")
-    sex=request.args.get("sex")
-    age=request.args.get("age")
-    Tel=request.args.get("Tel")
+    name = request.args.get("name")
+    sex = request.args.get("sex")
+    age = request.args.get("age")
+    Tel = request.args.get("Tel")
     print('你的签到情况：')
-    print('姓名：%s  年纪：%s  性别：%s  电话：%s'%(name,age,sex,Tel))
+    print('姓名：%s  年纪：%s  性别：%s  电话：%s' % (name, age, sex, Tel))
+
+    with open('签到表单.csv', 'a', newline='') as f:
+        csvwriter = csv.writer(f, dialect='excel')
+        # csvwriter.writerow(['姓名'], ['性别'], ['年龄'], ['电话'])
+        csvwriter.writerow([name, sex, age, Tel])
     return render_template('HTML4.html')
 
 
