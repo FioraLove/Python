ddd678531bcf0ea4b665597ccce7a6f869d8b0e1
FioraLove <51014054+FioraLove@users.noreply.github.com>
2019-06-13 11:37:22 +0800

    实现服务器与客户端可多次通信交流

--- Python_0_100天/day03_socket编程/socket服务端.py
+++ Python_0_100天/day03_socket编程/socket服务端.py
@@ -4,15 +4,15 @@ import socket
 server = socket.socket()
 server.bind(('localhost', 6969))  # 绑定要监听的接口
 server.listen()  # 监听
-
 print('我要开始等电话了')
 conn, addr = server.accept()  # 等待响应
 # conn就是客户端连过来而在服务端为其生成的一个连接实例
 print(conn, addr)
-
 print('电话来了')
-data = conn.recv(1024)
-print('recv:', data)
-conn.send(data.upper())
+while True:
+    data = conn.recv(1024)
+    print('recv:', data)
+    conn.send(data.upper())
 
-server.close()
+# server.close()
+
