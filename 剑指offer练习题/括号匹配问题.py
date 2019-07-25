25b7e1febaee39c494d5d860bbc6138365cbf72f
FioraLove <51014054+FioraLove@users.noreply.github.com>
2019-07-25 16:16:28 +0800

    完善栈的括号匹配问题
    Python的pop() 函数用于移除列表中的一个元素(默认最后一个元素)
    Python的appen() 函数用于在列表尾插入一个元素

--- 剑指offer练习题/括号匹配问题.py
+++ 剑指offer练习题/括号匹配问题.py
@@ -1,15 +1,20 @@
 # -*- coding:UTF-8 -*-
 def kuohao_match(exp):
+    # 以列表为栈
     stack = []
-    di = {'(': ')', '{': '}', '[': ']'}
-    for c in exp:
-        if c in {'(', '{', '['}:
-            stack.append(c)
+    # 目标条件匹配
+    items = {'(': ')', '{': '}', '[': ']'}
+    # 遍历目标字符串
+    for item in exp:
+        # 遍历item是否含有这三种字符
+        if item in {'(', '{', '['}:
+            stack.append(item)
         else:
             if len(stack) == 0:
                 return False
+            # 返回栈顶元素,pop() 函数用于移除列表中的一个元素(默认最后一个元素)
             top = stack.pop()
-            if di[top] != c:
+            if items[top] != item:
                 return False
     if len(stack) > 0:
         return False
